bb22c0b0042aef2a271feaf14304b005d4651d7a
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2022-06-17 09:19:45 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(554, music.beat(BeatFraction.Quarter))
+    music.playTone(698, music.beat(BeatFraction.Quarter))
+    music.playTone(831, music.beat(BeatFraction.Half))
+    music.playTone(698, music.beat(BeatFraction.Quarter))
+    music.playTone(831, music.beat(BeatFraction.Half))
+})
