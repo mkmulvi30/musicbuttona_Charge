76988040054931e7b184f007acacf9ef1091f1a7
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2022-06-17 09:20:57 -0500

    moreefficient

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
 input.onButtonPressed(Button.A, function () {
-    music.playTone(392, music.beat(BeatFraction.Quarter))
-    music.playTone(554, music.beat(BeatFraction.Quarter))
-    music.playTone(698, music.beat(BeatFraction.Quarter))
-    music.playTone(831, music.beat(BeatFraction.Half))
-    music.playTone(698, music.beat(BeatFraction.Quarter))
-    music.playTone(831, music.beat(BeatFraction.Half))
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
 })
